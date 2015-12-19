8bda570581340ec48a385fddd2a40f2ece4e3e18
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2015-12-19 18:32:11 +0200

    adding some changes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # forecast-api
+
+Simple wrapper for Forecast api in Go (work in progress)
+
+Checks the docs here: [https://developer.forecast.io/docs/v2](https://developer.forecast.io/docs/v2)
+
+```
+package main
+
+import (
+	"fmt"
+	forecast "github.com/sger/forecast-api"
+	"log"
+)
+
+func main() {
+
+	lat := "39.3677979"
+	lon := "22.9559652"
+
+	f, err := forecast.Search("api-key", lat, lon)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Forecast %d \n", f)
+	fmt.Printf("Summary %s \n", f.Currently.Summary)
+}
+```
+
+
--- forecast.go
+++ forecast.go
@@ -2,9 +2,48 @@ package forecast
 
 const URL_FORCAST_API = "https://api.forecast.io/forecast"
 
+type DataBlock struct {
+	Summary string      `json:"summary"`
+	Icon    string      `json:"icon"`
+	Data    []DataPoint `json:"data"`
+}
+
+type DataPoint struct {
+	Time                   float64 `json:"time"`
+	Summary                string  `json:"summary"`
+	Icon                   string  `json:"icon"`
+	SunriseTime            float64 `json:"SunriseTime"`
+	SunsetTime             float64 `json:"SunsetTime"`
+	PrecipIntensity        float64 `json:"precipIntensity"`
+	PrecipIntensityMax     float64 `json:"precipIntensityMax"`
+	PrecipIntensityMaxTime float64 `json:"precipIntensityMaxTime"`
+	PrecipProbability      float64 `json:"precipProbability"`
+	PrecipType             string  `json:"precipType"`
+	PrecipAccumulation     float64 `json:"precipAccumulation"`
+	Temperature            float64 `json:"temperature"`
+	TemperatureMin         float64 `json:"temperatureMin"`
+	TemperatureMinTime     float64 `json:"temperatureMinTime"`
+	TemperatureMax         float64 `json:"temperatureMax"`
+	TemperatureMaxTime     float64 `json:"temperatureMaxTime"`
+	ApparentTemperature    float64 `json:"apparentTemperature"`
+	DewPoint               float64 `json:"dewPoint"`
+	WindSpeed              float64 `json:"windSpeed"`
+	WindBearing            float64 `json:"windBearing"`
+	CloudCover             float64 `json:"cloudCover"`
+	Humidity               float64 `json:"humidity"`
+	Pressure               float64 `json:"pressure"`
+	Visibility             float64 `json:"visibility"`
+	Ozone                  float64 `json:"ozone"`
+	MoonPhase              float64 `json:"moonPhase"`
+}
+
 type Forecast struct {
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	Timezone  string  `json:"timezone"`
-	Offset    float64 `json:"offset"`
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	Timezone  string    `json:"timezone"`
+	Offset    float64   `json:"offset"`
+	Currently DataPoint `json:"currently"`
+	Minutely  DataBlock `json:"minutely"`
+	Hourly    DataBlock `json:"hourly"`
+	Daily     DataBlock `json:"daily"`
 }
